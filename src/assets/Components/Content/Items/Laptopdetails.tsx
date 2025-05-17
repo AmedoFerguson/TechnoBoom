84dd56f68752ceaef16eae7d5ba081ae5e435f10
AmedoFerguson <mustafaevalim9@gmail.com>
2025-05-17 20:32:34 +0300

    7333

--- src/assets/Components/Content/Items/Laptopdetails.tsx
+++ src/assets/Components/Content/Items/Laptopdetails.tsx
@@ -88,7 +88,7 @@ const LaptopDetails: React.FC<LaptopDetailsProps> = ({
 			<div className='ad'>Оголошення</div>
 			<div className='upper-laptop-details'>
 				<img
-					src={laptop?.image_url || '../../../../../public/null.png'}
+					src={laptop?.image_url || '/public/null.png'}
 					className='laptop-photo-details'
 					alt={laptop?.title || 'Ноутбук'}
 				/>
